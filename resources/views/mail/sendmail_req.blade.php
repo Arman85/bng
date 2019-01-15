5051f55f499c35c64a5e1e964450b621b25cedab
Arman85 <advanced315@gmail.com>
2019-01-15 15:09:17 +0600

    fix

--- resources/views/mail/sendmail_req.blade.php
+++ resources/views/mail/sendmail_req.blade.php
@@ -1,8 +1,8 @@
 
-<p><b>Заявка на консультацию с сайта: 	</b>	 <br>
-Имя: {{ $name }} <br>
-Телефон: {{ $phone }} <br>
-Когда позвонить: {{ $when }} <br>
-Язык: {{ $lang }} <br>
-Город: {{ $city }}</p>
+<p><b>Заявка на консультацию с сайта:</b></p>
+<p>Имя:&nbsp;<strong>{{ $name }}</strong></p>
+<p>Телефон:&nbsp;<strong>{{ $phone }}</strong></p>
+<p>Когда позвонить:&nbsp;<strong>{{ $when }} </strong></p>
+<p>Язык:&nbsp;<strong>{{ $lang }} </strong></p>
+<p>Город:&nbsp;<strong>{{ $city }}</strong></p>
 
